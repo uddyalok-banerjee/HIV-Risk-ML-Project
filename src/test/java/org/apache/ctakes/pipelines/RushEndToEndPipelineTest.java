75f75f10f82824dd950b81f068a2fca5581651c7
Brandon DeVries <bdevries@cloudera.com>
2020-04-08 13:15:38 -0400

    initial unit test

--- src/test/java/org/apache/ctakes/pipelines/RushEndToEndPipelineTest.java
+++ src/test/java/org/apache/ctakes/pipelines/RushEndToEndPipelineTest.java
@@ -1,7 +1,57 @@
 package org.apache.ctakes.pipelines;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.ctakes.utils.RushConfig;
+import org.junit.Rule;
+import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class RushEndToEndPipelineTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void name() {
+    }
+
+    @Test
+    public void testPipeline() throws Exception {
+
+        FileUtils.forceMkdir(new File("/tmp/random")); // required for current implementation...
+
+        File inputDirectory = Paths.get("src/test/resources/input").toFile();
+        File outputDirectory = Paths.get("src/test/resources/expectedOutput").toFile();
+
+        File masterFolder = Paths.get("resources").toFile();
+        File tempMasterFolder = folder.newFolder("tempMasterFolder");
+
+        RushConfig config = new RushConfig(masterFolder.getAbsolutePath(),
+                tempMasterFolder.getAbsolutePath());
+        config.initialize();
+        RushEndToEndPipeline pipeline = new RushEndToEndPipeline(config, true);
+
+        for (File file : inputDirectory.listFiles()) {
+            String t = FileUtils.readFileToString(file);
+            CTakesResult result = pipeline.getResult(file.getAbsolutePath(), 1, t);
+
+            String expectedOutput = FileUtils.readFileToString(new File(outputDirectory.getAbsolutePath() + "/xmis/" + file.getName()));
+            String expectedCuis = FileUtils.readFileToString(new File(outputDirectory.getAbsolutePath() + "/cuis/" + file.getName()));
+
+//            assertEquals(expectedOutput,result.getOutput());
+            assertEquals(expectedCuis,result.getCuis());
+
+            FileUtils.write(new File(new File(outputDirectory, "xmis"), file.getName()), result.getOutput());
+            FileUtils.write(new File(new File(outputDirectory, "cuis"), file.getName()), result.getCuis());
+        }
+        System.out.println("Closing Pipeline");
+        pipeline.close();
+        config.close();
+    }
 
 }
